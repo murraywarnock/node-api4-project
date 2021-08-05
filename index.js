940c75348ea3922d72775b53c8dc64f1a03ab9bc
MurrayWarnock <murray.warnock@gmail.com>
2021-08-05 16:03:18 -0600

    Cleanup

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ require('dotenv').config();
 
 const { PORT } = require('./config');
 const server = require('./api/server');
-// const express = require('express');
-// const cors = require('cors');
-// const helmet = require('helmet');
-
-// const server = express();
-
-// server.use(express.json());
-// server.use(cors());
-// server.use(helmet());
-
-// server.get('/', (req, res) => {
-//     res.send(`
-//     <h1>Mod 4 Project</h1>`)
-// })
-
-// server.use('*', (req, res) => {
-//     res.json({
-//         message: 'web 44 is awsome indeed'
-//     })
-// })
 
 server.listen(PORT, () => { 
     console.log(`listening on ${PORT}`)
