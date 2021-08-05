074caa2bda54388d0ce13b702d470b5ce43be1a7
MurrayWarnock <murray.warnock@gmail.com>
2021-08-05 13:10:03 -0600

    Node and server all in index.js returning an html get

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config(); //allows to inject fake env vars
+// const PORT = process.env.PORT || 5000;
+const { PORT } = require('./config');
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Mod 4 Project</h1>`)
+})
+
+server.use('*', (req, res) => {
+    res.json({
+        message: 'web 44 is awsome indeed'
+    })
+})
+
+server.listen(PORT, () => { //heroku machine uses a different port
+    console.log(`listening on ${PORT}`)
+});
+
